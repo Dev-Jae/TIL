e5f193d685265afe58afb7fe54eb32dfb9dc84cd
Dev-Jae <devhello940112@gmail.com>
2023-08-14 22:08:04 +0900

    RestTemplate naverApi 사용

--- FastCampusSpring/client/src/main/java/com/example/client/service/RestTemplateService.java
+++ FastCampusSpring/client/src/main/java/com/example/client/service/RestTemplateService.java
@@ -17,6 +17,12 @@ import java.net.URI;
 @Service
 @Slf4j
 public class RestTemplateService {
+    /*
+       UriComponents : URI를 구성하는 Components들을 효과적으로 다룰 수 있도록 하는 클래스
+       UriComponentsBuilder : UriComponents를 Build할 수 있도록 도와주는 클래스
+       RequestEntity : url 요청을 보낼 때 사용, header, body, method, url , type을 생성자 파라미터로 넘길 수 있음
+       ResponseEntity : url 요청에 응답할 때 사용, body, header, status를 생성자 파라미터로 넘길 수 있음
+     */
 
     // http://localhost/api/server/hello
     // response
@@ -32,7 +38,7 @@ public class RestTemplateService {
 
         log.info("uri : {}", uri.toString());
 
-        RestTemplate restTemplate = new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate(); // 간편하게 Rest 방식 API를 호출할 수 있는 Spring 내장 클래스
         ResponseEntity<UserResponse> result = restTemplate.getForEntity(uri, UserResponse.class);
 
         log.info("result code : {}", result.getStatusCode());
@@ -94,6 +100,8 @@ public class RestTemplateService {
 
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<UserResponse> response = restTemplate.exchange(requestEntity, UserResponse.class);
+        // exchange : HTTP 헤더를 새로 만들 수 있고 어떤 HTTP 메서드도 사용가능
+        // Get 요청에 header 값이 필요한 경우 exchange 메소드를 사용해 header 추가
 
         return response.getBody();
     }
@@ -137,6 +145,7 @@ public class RestTemplateService {
 
         ResponseEntity<Req<UserResponse>> response
                 = restTemplate.exchange(requestEntity,  new ParameterizedTypeReference<Req<UserResponse>>(){});
+        // ParameterizedTypeReference : Java 단에서 외부 서버의 Rest API로 얻어온 자원을 어떤 타입의 Java object로 변환할지를 미리 지정할 떄 사용
 
         return response.getBody();
     }
--- FastCampusSpring/server/src/main/java/com/example/server/dto/Req.java
+++ FastCampusSpring/server/src/main/java/com/example/server/dto/Req.java
@@ -5,8 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
+@AllArgsConstructor // 모든 필드 값에 대한 생성자를 생성
+@NoArgsConstructor  // 파라미터가 없는 기본 생성자 생성
 public class Req<T> {
 
     private Header header;
